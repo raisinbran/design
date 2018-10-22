53024ae83e0f3419e21820a8dd641bd744b18675
raisinbran <brandonjianchen@gmail.com>
2018-10-22 01:49:12 -0700

    20181022
    Engineeringdesign collapses, images, indices, descriptions updates

---
+++ auxiliary.js
@@ -0,0 +1,7 @@
+// Add custom JS here
+$('a[rel=popover]').popover({
+  html: true,
+  trigger: 'hover',
+  placement: 'bottom',
+  content: function(){return '<img src="'+$(this).data('img') + '" />';}
+});
